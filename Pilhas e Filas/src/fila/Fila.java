4561934ced4cf0222512d8b9b64930ba64761441
Kevin Nunes <58992523+KeviNunes@users.noreply.github.com>
2021-01-23 16:03:29 -0300

    Arrumando a identação

--- Pilhas e Filas/src/fila/Fila.java
+++ Pilhas e Filas/src/fila/Fila.java
@@ -5,24 +5,24 @@ import java.util.List;
 
 public class Fila {
 	//Cria a lista encadeada
-		List<Object> fila = new LinkedList<Object>();
+	List<Object> fila = new LinkedList<Object>();
 		
-		//Método que adiciona elementos na fila
-		public void add(Object elemento) {
-			fila.add(elemento);
-		}
-		
-		//Método que remove os elementos da fila
-		public Object remove() {
-			return fila.remove(0);
-		}
-		
-		//Método que verifica se a fila está vazia
-		public boolean empty() {
-			return fila.isEmpty();
-		}
-		
-		public String toString() {
-			return fila.toString();
-		}
+	//Método que adiciona elementos na fila
+	public void add(Object elemento) {
+		fila.add(elemento);
+	}
+
+	//Método que remove os elementos da fila
+	public Object remove() {
+		return fila.remove(0);
+	}
+
+	//Método que verifica se a fila está vazia
+	public boolean empty() {
+		return fila.isEmpty();
+	}
+
+	public String toString() {
+		return fila.toString();
+	}
 }
